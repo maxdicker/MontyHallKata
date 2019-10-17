9b6ecdeb7f13ee63125e3e696241fb1c724a3f6b
max.dicker <max.dicker@myob.com>
2019-10-17 12:34:42 +1100

    Add prize count test

--- src/test/java/com/MontyHallTest.java
+++ src/test/java/com/MontyHallTest.java
@@ -20,12 +20,28 @@ public class MontyHallTest {
     }
 
     @Test
-    public void doorFactoryCreatesSpecifiedNumberOfDoors() {
+    public void doorFactoryCanCreatesSpecifiedNumberOfDoors() {
         DoorFactory factory = new DoorFactory(new RealRandom());
 
         var doors = factory.createDoors(3);
 
         assertEquals(3, doors.length);
+    }
+
+    @Test
+    public void doorFactoryCreatesExactlyOneDoorContainingPrize() {
+        DoorFactory factory = new DoorFactory(new RealRandom());
+
+        var doors = factory.createDoors(5);
+
+        int numberOfPrizes = 0;
+        for (Door door : doors) {
+            if (door.hasPrize()) {
+                numberOfPrizes++;
+            }
+        }
+
+        assertEquals(1, numberOfPrizes);
     }
 
 
