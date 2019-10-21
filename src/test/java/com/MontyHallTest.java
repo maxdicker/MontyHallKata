39606019c686e0ab30829ff35902dbf75a4cfe5d
max.dicker <max.dicker@myob.com>
2019-10-21 15:21:28 +1100

    Refactor tests

--- src/test/java/com/MontyHallTest.java
+++ src/test/java/com/MontyHallTest.java
@@ -1,25 +1,16 @@
 package com;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.List;
+
 public class MontyHallTest {
 
-    // Bad typing of percentage. Susceptible to rounding and assertion issues.
     @Test
-    public void loggerReturnsWinPercentage() {
-        ResultLogger logger = new ResultLogger();
-
-        logger.logBoolResult(true);
-        logger.logBoolResult(false);
-        logger.logBoolResult(true);
-
-        assertEquals((double)2/3*100, logger.getPercentageOfTrue(), 0);
-    }
-
-    @Test
-    public void doorFactoryCreatesSpecifiedNumberOfDoors() {
+    public void doorFactoryReturnsSpecifiedNumberOfDoors() {
         DoorFactory factory = new DoorFactory(new RealRandom());
 
         var doors = factory.createDoors(3);
@@ -28,42 +19,90 @@ public class MontyHallTest {
     }
 
     @Test
-    public void doorFactoryCreatesExactlyOneDoorContainingPrize() {
+    public void doorFactoryReturnsExactlyOneDoorContainingPrize() {
         DoorFactory factory = new DoorFactory(new RealRandom());
 
         var doors = factory.createDoors(5);
-
-        int numberOfPrizes = 0;
-        for (Door door : doors) {
-            if (door.hasPrize) {
-                numberOfPrizes++;
-            }
-        }
+        int numberOfPrizes = findNumberOfPrizes(doors);
 
         assertEquals(1, numberOfPrizes);
     }
 
+    //Its not clear that this gives player the prize door. Need to know implementation.
     @Test
-    public void givenSimulationWherePlayerDoesntSwitch_WhenPlayerStartsWithPrize_PlayerShouldWinAllGames() {
+    public void wherePlayerStartsWithPrizeDoor_ifSimulatingThatPlayerDoesntSwitch_PlayerShouldWinAllGames() {
         IRandom stub = new StubRandom(0);
         MontyHallSimulator simulator = new MontyHallSimulator(stub, false);
-        simulator.simulateMontyHall(1000);
 
+        simulator.simulateMontyHall(1000);
         int wins = simulator.getNumberOfPlayerWins();
 
         assertEquals(1000, wins);
     }
 
     @Test
-    public void givenSimulationWherePlayerSwitches_WhenPlayerStartsWithPrize_PlayerShouldLoseAllGames() {
+    public void wherePlayerStartsWithPrizeDoor_ifSimulatingThatPlayerSwitches_PlayerShouldLoseAllGames() {
         IRandom stub = new StubRandom(0);
         MontyHallSimulator simulator = new MontyHallSimulator(stub, true);
-        simulator.simulateMontyHall(1000);
 
+        simulator.simulateMontyHall(1000);
         int wins = simulator.getNumberOfPlayerWins();
 
         assertEquals(0, wins);
     }
 
+    //Could also test the last two scenarios (where player doesn't start with prize doors), but this won't improve coverage?
+
+    @Test
+    public void wherePlayerDoesntStartWithPrizeDoor_ifSimulatingThatPlayerDoesntSwitch_PlayerShouldLoseAllGames() {
+        IRandom stub = new StubRandom(1);
+        MontyHallSimulator simulator = new MontyHallSimulator(stub, false);
+
+        simulator.simulateMontyHall(1000);
+        int wins = simulator.getNumberOfPlayerWins();
+
+        assertEquals(0, wins);
+    }
+
+    @Test
+    public void wherePlayerDoesntStartWithPrizeDoor_ifSimulatingThatPlayerSwitches_PlayerShouldWinAllGames() {
+        IRandom stub = new StubRandom(1);
+        MontyHallSimulator simulator = new MontyHallSimulator(stub, true);
+
+        simulator.simulateMontyHall(1000);
+        int wins = simulator.getNumberOfPlayerWins();
+
+        assertEquals(1000, wins);
+    }
+
+    @Test
+    public void simulatingThatPlayerSwitchesDoors_PlayerShouldWinMostGames() {
+        MontyHallSimulator simulator = new MontyHallSimulator(new RealRandom(), true);
+
+        simulator.simulateMontyHall(1000);
+        int wins = simulator.getNumberOfPlayerWins();
+
+        assertTrue(wins > 620 && wins < 720);
+    }
+
+    @Test
+    public void simulatingThatPlayerDoesntSwitchDoors_PlayerShouldLoseMostGames() {
+        MontyHallSimulator simulator = new MontyHallSimulator(new RealRandom(), false);
+
+        simulator.simulateMontyHall(1000);
+        int wins = simulator.getNumberOfPlayerWins();
+
+        assertTrue(wins < 390 && wins > 290);
+    }
+
+    private int findNumberOfPrizes(List<Door> doors) {
+        int numberOfPrizes = 0;
+        for (Door door : doors) {
+            if (door.hasPrize) {
+                numberOfPrizes++;
+            }
+        }
+        return numberOfPrizes;
+    }
 
 }
