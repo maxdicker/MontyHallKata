8e2d9b68a0deaba721c095ca5c4c805db12bdbbd
Dennis Ma <dennis.ma@myob.com>
2019-10-23 15:38:51 +1100

    Avoid repeating the hard-coded number of simulations constant and make the percentages not relying on exactly 1000 simulations

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,20 +1,25 @@
 package com;
 
+import java.text.DecimalFormat;
+
 public class Main {
+
+    private static final int NUMBER_OF_SIMULATIONS = 1000;
+    private static final DecimalFormat df = new DecimalFormat("##.##%");
 
     public static void main(String[] args) {
         IO io = new ConsoleIO();
         io.printLine("Welcome to Monty Hall!");
 
         MontyHallSimulator simPlayerSwitching = new MontyHallSimulator(new RandomScenario(), true);
-        simPlayerSwitching.simulateMontyHall(1000);
+        simPlayerSwitching.simulateMontyHall(NUMBER_OF_SIMULATIONS);
         int switchWins = simPlayerSwitching.getNumberOfPlayerWins();
-        io.printLine("Simulating 1000 games where the player SWITCHES their door, we found they won " + switchWins/10 + "%.");
+        io.printLine("Simulating "+ NUMBER_OF_SIMULATIONS +" games where the player SWITCHES their door, we found they won " + df.format(switchWins * 1.0f /NUMBER_OF_SIMULATIONS)+".");
 
         MontyHallSimulator simPlayerNotSwitching = new MontyHallSimulator(new RandomScenario(), false);
-        simPlayerNotSwitching.simulateMontyHall(1000);
+        simPlayerNotSwitching.simulateMontyHall(NUMBER_OF_SIMULATIONS);
         int holdWins = simPlayerNotSwitching.getNumberOfPlayerWins();
-        io.printLine("Simulating 1000 games where the player DOESN'T SWITCH their door, we found they won " + holdWins/10 + "%.");
+        io.printLine("Simulating "+ NUMBER_OF_SIMULATIONS +" games where the player DOESN'T SWITCH their door, we found they won " + df.format(holdWins * 1.0f /NUMBER_OF_SIMULATIONS)+".");
 
     }
 }
